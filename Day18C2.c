a2679a652f15f393bf91b06ab59d796d7f906eed
TechzyAdi <ajtechybro@gmail.com>
2026-02-18 10:21:02 +0530

    Implement product ExceptSelf function

---
+++ Day18C2.c
@@ -0,0 +1,25 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+#include <stdlib.h>
+
+int* productExceptSelf(int* nums, int numsSize, int* returnSize) {
+    
+    int* answer = (int*)malloc(numsSize * sizeof(int));
+    *returnSize = numsSize;
+    
+    // Step 1: Store prefix products in answer
+    answer[0] = 1;
+    for(int i = 1; i < numsSize; i++) {
+        answer[i] = answer[i - 1] * nums[i - 1];
+    }
+    
+    // Step 2: Multiply with suffix products
+    int suffix = 1;
+    for(int i = numsSize - 1; i >= 0; i--) {
+        answer[i] = answer[i] * suffix;
+        suffix = suffix * nums[i];
+    }
+    
+    return answer;
+}
