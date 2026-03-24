286d594c9058cdcfd8d8af5f1bac9abe00400bf2
TechzyAdi <ajtechybro@gmail.com>
2026-03-24 16:39:19 +0530

    Lowest Common Ancestor of a BST

---
+++ Day51C2.c
@@ -0,0 +1,13 @@
+struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q) 
+{
+    // If both nodes are smaller → go left
+    if(p->val < root->val && q->val < root->val)
+        return lowestCommonAncestor(root->left, p, q);
+
+    // If both nodes are greater → go right
+    if(p->val > root->val && q->val > root->val)
+        return lowestCommonAncestor(root->right, p, q);
+
+    // Split point → this is LCA
+    return root;
+}
