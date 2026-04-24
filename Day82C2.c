3fd3e87df872c87b651f0f6a9f6e19e3ee8b1b1f
TechzyAdi <ajtechybro@gmail.com>
2026-04-24 22:32:59 +0530

    Implement binary search for insert position

---
+++ Day82C2.c
@@ -0,0 +1,17 @@
+int searchInsert(int* nums, int numsSize, int target) {
+    int left = 0, right = numsSize - 1;
+    int ans = numsSize;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] >= target) {
+            ans = mid;
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return ans;
+}
